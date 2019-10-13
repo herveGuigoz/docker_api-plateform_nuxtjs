fcfd80a1d5307e9948c253c5aebe08011a350029
herveGuigoz <herve.guigoz@gmail.com>
2019-10-13 23:19:13 +0200

    first commit

--- scripts/linux/uninstall.sh
+++ scripts/linux/uninstall.sh
@@ -1,4 +1,4 @@
 #Uninstall all about docker containers and volumes.
-docker rm $(docker ps -a -q -f name=familly_cooking)
+docker rm $(docker ps -a -q -f name=projectName)
 docker rmi $(docker images -q)
 rm -rf .docker
